7509262089185fa38376eddc9712aebcb98d75bb
rahul <rahul08yaduvanshi@gmail.com>
2023-10-02 16:26:31 +0530

    added footer

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import styles from './pagination.module.css';
+import React from "react";
+import styles from "./pagination.module.css";
 
 function Pagination() {
-  return <div className={styles.container}>Pagination</div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.button}>Previous</button>
+      <button className={styles.button}>Next</button>
+    </div>
+  );
 }
 
 export default Pagination;
