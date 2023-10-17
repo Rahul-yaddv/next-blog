0e53f814937689a1bb850d17ddab74ab6135600d
Rahul-yaddv <107284988+Rahul-yaddv@users.noreply.github.com>
2023-10-18 04:25:10 +0530

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,12 +6,37 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE,
-  authDomain: "blog-cddc2.firebaseapp.com",
-  projectId: "blog-cddc2",
-  storageBucket: "blog-cddc2.appspot.com",
-  messagingSenderId: "413696194974",
-  appId: "1:413696194974:web:bc0a551cd5558bdf0dbe8f"
+  authDomain: "blog-c31db.firebaseapp.com",
+  projectId: "blog-c31db",
+  storageBucket: "blog-c31db.appspot.com",
+  messagingSenderId: "939367439607",
+  appId: "1:939367439607:web:3c8e5e5b875e720d3007f2"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE,
+//   authDomain: "blog-cddc2.firebaseapp.com",
+//   projectId: "blog-cddc2",
+//   storageBucket: "blog-cddc2.appspot.com",
+//   messagingSenderId: "413696194974",
+//   appId: "1:413696194974:web:bc0a551cd5558bdf0dbe8f"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
