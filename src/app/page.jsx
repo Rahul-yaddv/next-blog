189efda4b284c1d7179954a340fee3c879d0760a
rahul <rahul08yaduvanshi@gmail.com>
2023-10-31 17:28:18 +0530

    misc

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,17 +3,13 @@ import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/Menu/Menu";
-import Loading from "@/components/Loading/Loading";
-import { Suspense } from "react";
 
 export default function Home({ searchParams }) {
   const page = parseInt(searchParams.page) || 1;
 
   return (
     <div className={styles.container}>
-      <Suspense fallback={<Loading />}>
-        <Featured />
-      </Suspense>
+      <Featured />
       <CategoryList />
       <div className={styles.content}>
         <CardList page={page} />
