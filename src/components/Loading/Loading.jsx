1642bf632617735aa8364fc1bf1d77ab79ce3b28
rahul <rahul08yaduvanshi@gmail.com>
2023-10-31 17:09:17 +0530

    misc

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,4 +1,4 @@
 import styles from "./loading.module.css";
 export default function loading() {
-  return <div className={styles.lds - roller}></div>;
+  return <div className={styles.ldsRoller}></div>;
 }
